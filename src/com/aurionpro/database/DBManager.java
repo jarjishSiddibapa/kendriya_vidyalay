ec5e91d55f69ab3880fa2d113c9c1e0f9708b219
Dheeren Gaud <ganeshgaud1111@gmail.com>
2025-07-29 00:56:44 +0530

    CourseManagnment Done!! Virsion-02

--- src/com/aurionpro/database/DBManager.java
+++ src/com/aurionpro/database/DBManager.java
@@ -18,7 +18,7 @@ public class DBManager {
 	}
 	public boolean isMapExist(int id1,int id2,Table_ID id1_name,Table_ID id2_name,Table_Map table_Map) {
 		try {
-			prepareStatement = connection.prepareStatement("select 1 from "+table_Map+" where " + id1_name + "=? and "+id2_name +"=?");
+			prepareStatement = connection.prepareStatement("select 1 from "+table_Map+" where " + id1_name + "=? and "+id2_name +"=? and  is_active = TRUE");
 			prepareStatement.setInt(1, id1);
 			prepareStatement.setInt(2, id2);
 			ResultSet result = prepareStatement.executeQuery();
@@ -34,7 +34,8 @@ public class DBManager {
 	
 	public boolean isExist(int id ,Table_ID table_id,Table table ) {
 		try {
-			prepareStatement = connection.prepareStatement("select 1 from "+table+" where " + table_id + "=?");
+
+			prepareStatement = connection.prepareStatement("select 1 from "+table+" where " + table_id + "=? and is_active = TRUE");
 			prepareStatement.setInt(1, id);
 
 			ResultSet result = prepareStatement.executeQuery();
@@ -47,20 +48,28 @@ public class DBManager {
 			return false;
 		}
 	}
-	public void delete(int id ,Table_ID table_id,Table table ) {
-		try {
-				prepareStatement = connection.prepareStatement("delete from "+table+" where "+table_id+" = ?");
-				prepareStatement.setInt(1, id);
-				int update = prepareStatement.executeUpdate();
-				System.out.println(update + " "+table+" Deleted  successfully");
-		}catch (SQLException e) {
-			e.printStackTrace();
-		}
+	public void delete(int id, Table_ID table_id, Table table) {
+	    try {
+	        String sql = "UPDATE " + table + " SET is_active = FALSE WHERE " + table_id + " = ?";
+	        prepareStatement = connection.prepareStatement(sql);
+	        prepareStatement.setInt(1, id);
+	        int update = prepareStatement.executeUpdate();
+	        
+	        if (update > 0) {
+	            System.out.println(update + " record(s) soft-deleted from " + table + " successfully.");
+	        } else {
+	            System.out.println("No matching record found in " + table + " with ID: " + id);
+	        }
+
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
 	}
+
 
 	public boolean isStringExist(String str, String column_name, Table table) {
 		try {
-			prepareStatement = connection.prepareStatement("select 1 from "+table+" where " + column_name + "=?");
+			prepareStatement = connection.prepareStatement("select 1 from "+table+" where " + column_name + "=? and  is_active = TRUE");
 			prepareStatement.setString(1, str);
 
 			ResultSet result = prepareStatement.executeQuery();
